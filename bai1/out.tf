f0a790a04264f9313162ceeef3dcc826a12002b9
pkl <pnthanh22121999@gmail.com>
2022-10-03 22:14:51 +0700

    baif 1

---
+++ bai1/out.tf
@@ -0,0 +1,8 @@
+//Chuyển tất cả output variable vào đây
+output "ssh_command" {
+  value = "ssh -i '${var.keyname}.pem' ec2-user@${aws_instance.web.public_ip}"
+}
+
+output "web_site" {
+  value = "http://${aws_instance.web.public_ip}"
+}
---
+++ bai1/var.tf
@@ -0,0 +1,5 @@
+variable "keyname" {
+  type        = string
+  default     = "demoky"
+  description = "name of keypair"
+}
